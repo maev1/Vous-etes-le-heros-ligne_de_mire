9fa644b177905e67743efd631e457cba55e47984
maev1 <maite.venne@icloud.com>
2022-09-29 11:19:59 -0400

    290922

---
+++ script.js
@@ -0,0 +1,85 @@
+let chapitre1 = {
+    titre : "arrivée",
+    description : "Vous arrivez dans le pays où vous allez vous battre afin de défendre votre pays pour les prochaines semaines et vous avez le choix entre aller vous reposez pour reprendre des force avant d'aller vous battre ou bien d'aller directement sur le champ de bataille.",
+    img : ,
+};
+
+let chapitre2 = {
+    titre : "allons se battre",
+    description : "Vous avez décidé d'aller vous battre au lieu de vous reposer, vous êtes en route vers le terrain.",
+    img : ,
+};
+
+let chapitre3 = {
+    titre : "repos",
+    description : "Vous avez décidé de rester au campement pour vous reposer mais lors de votre repos, le camps c'est fait attaquer et vous avez sucombé à vos blessure.",
+    img : ,
+};
+
+let chapitre4 = {
+    titre : "la guerre",
+    description : "Vous êtes en guerre et vous défendez!",
+    img : ,
+};
+
+let chapitre5 = {
+    titre : "l'enfant",
+    description : "Vous vous défendez et soudainement repérer un enfant dans votre ligne demire, celui-ci pourte une grenade et se dirige vers vos coéquipiers.",
+    img : ,
+};
+
+let chapitre6 = {
+    titre : "Ignorer",
+    description : "Vous avez ignoré l'enfant et il a réussi à tuer et blesser gravement vos coéquipiers, vous ne pouvez pas vous dfendre avec aussi peu d'allier. Vous vous faites tuer.",
+    img : ,
+};
+
+let chapitre7 = {
+    titre : "près de la victoire",
+    description : "Vous avez tué l'enfant et vos amis ne sont plus en danger, vous devez fuire rapidement avant que les renforts ennemis arrivent.",
+    img : ,
+};
+
+let chapitre8 = {
+    titre : "la fuite",
+    description : "Les ennemis s'approchent qu'allez vous faire?",
+    img : ,
+};
+
+let chapitre9 = {
+    titre : "l'attente",
+    description : "Vous avez attendu trop longtemps avant de fuire et les forces ennemis vous ont rattrapé. vous êtes mort.",
+    img : ,
+};
+
+let chapitre10 = {
+    titre : "prendre le matériel",
+    description : "Vous prenez le matériel avant de fuire.",
+    img : ,
+};
+
+
+let chapitre11 = {
+    titre : "se battre avant la fuite",
+    description : "Vous continuer de vous battre avant de prendre la fuite.",
+    img : ,
+};
+
+let chapitre12 = {
+    titre : "l'embuscade",
+    description : "Vous vous retrouvez dans une embuscade en vous dirigeant vers votre campement, vous avez choisi de prendre le matériel, ce qui vous a permis d'avoir les munitions nécessaire.",
+    img : ,
+};
+
+let chapitre13 = {
+    titre : "victoire",
+    description : "Vous avez réussi a survivre félicitation!",
+    img : ,
+};
+
+let chapitre14 = {
+    titre : "la défaite",
+    description : "Vous y étiez presque mais malheureusement vous êtes mort dans l'embuscade.",
+    img : ,
+};
+
